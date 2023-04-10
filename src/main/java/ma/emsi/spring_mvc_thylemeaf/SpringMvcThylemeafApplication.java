a32d05b6357ed4f5c91b4b82ec720327ea4c6ecd
Othman Nassik <othmannassik1@gmail.com>
2023-04-10 00:51:39 +0000

    Project Setup

---
+++ src/main/java/ma/emsi/spring_mvc_thylemeaf/SpringMvcThylemeafApplication.java
@@ -0,0 +1,13 @@
+package ma.emsi.spring_mvc_thylemeaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcThylemeafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcThylemeafApplication.class, args);
+    }
+
+}
